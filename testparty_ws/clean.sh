187cd713e56fc3749f50c6072f5445689eabff4b
slowrunner <slowrunner@users.noreply.github.com>
2026-05-12 17:19:38 -0400

    moving to ssd

--- testparty_ws/clean.sh
+++ testparty_ws/clean.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+
 cd ~/U26LLPi5/testparty_ws
 
 echo "Cleaning testparty_ws (log, install, build)"
@@ -7,5 +9,10 @@ echo "Cleaning testparty_ws (log, install, build)"
 rm -rf log
 rm -rf install
 rm -rf build
+
+echo "Cleaning AMENT and CMAKE PREFIX paths"
+unset AMENT_PREFIX_PATH
+unset AMENT_CMAKE_PREFIX_PATH
+unset CMAKE_PREFIX_PATH
 
 echo "Cleaned"
